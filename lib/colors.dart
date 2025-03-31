78bcfcf8ddfd1444c2c21b3b19bc47cdb50c4104
Dhruvit Gopani <113504480+DhruvitGopani0041@users.noreply.github.com>
2025-03-31 16:53:32 +0530

    First commit

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color brownLight = Color(0xffc09c88);
+Color brownDark = Color(0xFF81554E);
+Color greenLight = Color(0xffA3DB72);
+Color greenDark = Color(0xff497E19);
+Color toGreen = Color(0xff033B2B);
+Color appGreen = Color(0xff067655);
+
+Color jj = Color(0xff033B2B);
+Color dd = Color(0xff067655);Color toGeereen = Color(0xff033B2B);
+Color ff = Color(0xff067655);Color toGrsdeen = Color(0xff033B2B);
+Color fg = Color(0xff067655);
